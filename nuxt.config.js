f8871c205fe5a11d9f5041acac523340489067e6
mninozebra <mni@nozebra.dk>
2020-01-12 13:12:16 +0100

    Updated nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,29 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.4.1.min.js', defer: true, body: false },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js', defer: true, body: false },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.1.0/jquery-migrate.min.js', defer: true, body: false },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js', defer: true, body: false },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js', defer: true, body: false },
+      { src: 'https://cdn.jsdelivr.net/npm/jquery.stellar@0.6.2/jquery.stellar.min.js', defer: true, body: false },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js', defer: true, body: false },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', defer: true, body: false },
+      { src: 'https://cdn.jsdelivr.net/combine/npm/jquery.stellar@0.6.2/jquery.stellar.min.js,npm/scrollax@1.0.0', defer: true, body: false }
+    ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css', async: true, body: true },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css', async: true, body: true },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/icomoon@1.0.0/style.min.css', async: true, body: true },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css', async: true, body: true },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css', async: true, body: true },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', async: true, body: true },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css', async: true, body: true }
+
+
     ]
   },
   /*
@@ -23,6 +44,8 @@ export default {
   ** Global CSS
   */
   css: [
+    '@/assets/css/style.css'
+
   ],
   /*
   ** Plugins to load before mounting the App
